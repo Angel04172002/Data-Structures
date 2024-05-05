3c304cca56c2ce7a65e5fa6a424b3fb552ea95ea
Angel1980 <58862906+Angel04172002@users.noreply.github.com>
2024-05-05 21:25:59 +0300

    Create Node.cs

---
+++ 2. Linked List/4. NthNode/Node.cs
@@ -0,0 +1,14 @@
+namespace LinkedListExercises
+{
+    public class Node
+    {
+        public Node(int value)
+        {
+            Value = value;
+        }
+
+        public int Value { get; set; }
+
+        public Node Next { get; set; }
+    }
+}
